79d36210b37bb5b4c990f68f10400e071e0449a5
ahmad farizy <ahmad@code.id>
2019-11-19 07:49:14 +0700

    Initial Commit

---
+++ Page View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Page View
+//
+//  Created by MAC on 19/11/19.
+//  Copyright © 2019 frzy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
